369780cb9d341b6203a2422fe0aa6f338e4a5c52
babbu <babbu@apptium.com>
2021-09-24 00:30:50 +0530

    changes

--- src/app/prime-signin/prime-signin.component.ts
+++ src/app/prime-signin/prime-signin.component.ts
@@ -13,22 +13,24 @@ export class PrimeSigninComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+
     const second = 1000,
         minute = second * 60,
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "Sep 22, 2021 14:00:00",
+  let birthday = "Sep 24, 2021 14:00:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
 
         let now = new Date().getTime(),
             distance = countDown - now;
-
-        document.getElementById("days").innerText = Math.floor(distance / (day)).toString(),
+        if(document.getElementById("days")){
+          document.getElementById("days").innerText = Math.floor(distance / (day)).toString(),
           document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)).toString(),
           document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)).toString(),
           document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second).toString();
+        }
 
         //do something later when date is reached
         if (distance < 0) {
