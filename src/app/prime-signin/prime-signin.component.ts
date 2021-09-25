d80e7ee6b223c76db145f90f48dcec2d33b5bc34
Nitish <nitish@APPLEs-MacBook-Pro.local>
2021-09-25 21:59:53 +0530

    prod

--- src/app/prime-signin/prime-signin.component.ts
+++ src/app/prime-signin/prime-signin.component.ts
@@ -28,7 +28,7 @@ export class PrimeSigninComponent implements OnInit {
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "Sep 25, 2021 20:00:00",
+  let birthday = "Sep 26, 2021 10:30:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
 
